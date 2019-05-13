6d34ffc45e96aae0f2097eae53ce7762518d7740
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-05-13 20:50:59 -0300

    isolamos o codigo de analise de log em uma funcao

--- linuxlibre.py
+++ linuxlibre.py
@@ -1,17 +1,21 @@
-URL = "http://linux-libre.fsfla.org/pub/linux-libre/releases/2.6.32.66-gnu1/linux-libre-2.6.32.66-gnu1.log"
-
 import requests
 
-text = requests.get(URL).text
+def analisa_log(url):
+  text = requests.get(url).text
+  modules = {}
 
-modules = {}
+  for line in text.split('\n'):
+    if " - " in line:
+      partes = line.split(" - ")
+      nome = partes[0]
+      desc = partes[1]
+      if nome not in modules and len(nome) > 2 and nome.isupper():
+        modules[nome] = desc
 
-for line in text.split('\n'):
-  if " - " in line:
-    partes = line.split(" - ")
-    nome = partes[0]
-    desc = partes[1]
-    if nome not in modules and len(nome) > 2 and nome.isupper():
-      modules[nome] = desc
-      print (f"{nome}:\t {desc}")
+  return modules
+
+
+m = analisa_log("http://linux-libre.fsfla.org/pub/linux-libre/releases/2.6.32.66-gnu1/linux-libre-2.6.32.66-gnu1.log")
+for nome, desc in m.items():
+  print (f"{nome}:\t {desc}")
 
