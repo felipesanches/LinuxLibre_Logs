01ffb62e88e2f9b8d556495e251aa0659841a34c
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-05-13 21:00:04 -0300

    implementamos uma listagem de todos logs de releases do linuxlibre

--- linuxlibre.py
+++ linuxlibre.py
@@ -1,5 +1,17 @@
 import requests
 
+def lista_logs():
+  BASE_URL = "http://linux-libre.fsfla.org/pub/linux-libre/releases"
+  html = requests.get(BASE_URL).text
+  logs = []
+  for line in html.split('\n'):
+    if 'href="' in line:
+      log_url = line.split('href="')[1].split('"')[0]
+      if "-gnu" in log_url:
+        logs.append(f"{BASE_URL}/{log_url}")
+  return logs
+
+
 def analisa_log(url):
   text = requests.get(url).text
   modules = {}
@@ -14,6 +26,9 @@ def analisa_log(url):
 
   return modules
 
+
+logs = lista_logs()
+print (logs)
 
 m = analisa_log("http://linux-libre.fsfla.org/pub/linux-libre/releases/2.6.32.66-gnu1/linux-libre-2.6.32.66-gnu1.log")
 for nome, desc in m.items():
